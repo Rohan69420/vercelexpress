5eeb57d4573746c5166371ff3500af05c20b0f35
Rohan <rohan13maharjan@gmail.com>
2024-09-10 13:47:11 +0545

    added additional corsoptions

--- server/api/index.js
+++ server/api/index.js
@@ -1,41 +1,47 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
-const path = require('path');
+const path = require("path");
 
 const app = express();
 const cors = require("cors");
 const corsOption = {
-origin: ["http://localhost:5173","https://vercelexpress-three.vercel.app:3000/","https://*"],
+  origin: [
+    "http://localhost:5173",
+    "https://vercelexpress-three.vercel.app:3000/",
+    "https://*",
+  ],
+  methods: ["GET", "POST", "OPTIONS"],
+  allowedHeaders: ["Content-Type"],
 };
 
 app.use(cors(corsOption));
 app.use(bodyParser.json());
 
 app.post("/verifycode", (req, res) => {
-console.log("POST parameter received are: ", req.body);
+  console.log("POST parameter received are: ", req.body);
 
-const { array } = req.body;
+  const { array } = req.body;
 
-// Server validation criteria : bad code if ends with 7 or less than length of 6
-if (!array || !Array.isArray(array) || array.length < 6 || array[5] === "7") {
+  // Server validation criteria : bad code if ends with 7 or less than length of 6
+  if (!array || !Array.isArray(array) || array.length < 6 || array[5] === "7") {
     return res.status(400).json({ error: "Invalid Code" });
-}
+  }
 
-// Respond with a success message
-res
+  // Respond with a success message
+  res
     .status(200)
     .json({ message: "Code Verified Successfully", receivedArray: array });
 });
 
 app.get("/", (req, res) => res.send("Express on Vercel"));
 
-app.get("/test",(req, res) => {
-    res.send(' The Server Get is running fine!');
-  });
+app.get("/test", (req, res) => {
+  res.send(" The Server Get is running fine!");
+});
 
 app.listen(3000, () => {
-console.log("Server started at port 3000");
+  console.log("Server started at port 3000");
 });
 
 module.exports = app;
